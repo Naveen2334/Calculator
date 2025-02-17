e325f9c21bc825e530287a6f71a05b1ba766c452
Naveen Kumar Barnwal <naveenkumar16613@gmail.com>
2025-02-17 23:44:54 +0530

    Hi I am Sajeer committing this file

--- Sajeer.java
+++ Sajeer.java
@@ -1,12 +1,12 @@
-public class abc{
+public class Sajeer {
     public static void main(String[] args){
         System.out.println("hello world");
         int a = 10;
         int b = 20;
-        System.out.println(a+b);
+       
         System.out.println(a-b);
-        System.out.println(a%b);
-        System.out.println("code first commit");
+   
+        System.out.println("Sajeer code first commit");
 
     }
 }
