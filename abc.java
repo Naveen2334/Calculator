33529cd416dc3d0337f40e76466288a805e1b5b2
Naveen Kumar Barnwal <naveenkumar16613@gmail.com>
2025-02-17 19:11:43 +0530

    first commit

---
+++ abc.java
@@ -0,0 +1,10 @@
+public class abc{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+        System.out.println("code first commit");
+
+    }
+}
